86c4a3082f096ea0f5bdabe95172f78936b7bb18
Neoblishange <74345944+Neoblishange@users.noreply.github.com>
2021-06-09 23:53:58 +0200

    1

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
+//Nav bar feedback
 function navClick(NavButton) {
     document.getElementById("active").removeAttribute("id");
     NavButton.id = "active";
 }
 
+//back to home on refresh
 window.onbeforeunload = function () {
     window.setTimeout(function () {
         window.location = "index.html";
